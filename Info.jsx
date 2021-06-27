b8d60dcc81fabc14738d8d06342e2eee4e255935
SaruSStar <54907171+SaruSStar@users.noreply.github.com>
2021-06-27 21:15:30 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React js</h1>
+      <p>Full Stack Web Development Bootcamp - HTML, CSS and Javascript</p>
+    </div>
+  );
+}
+
+export default Info;
